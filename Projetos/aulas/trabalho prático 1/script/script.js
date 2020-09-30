440d1f052efdf4d3607410324aebab8e6e533904
maclararose <maria.uespi@outlook.com>
2020-09-29 22:43:03 -0300

    RGB simulator

---
+++ Projetos/aulas/trabalho prático 1/script/script.js
@@ -0,0 +1,42 @@
+window.addEventListener('load', start);
+
+var inputRed = null;
+var rangeRed = null;
+var inputGreen = null;
+var rangeGreen = null;
+var inputBlue = null;
+var rangeBlue = null;
+var divSquare = null;
+
+function start(){
+    divSquare = document.querySelector('#square');
+
+    rangeRed = document.querySelector('#rangeRed');
+    rangeGreen = document.querySelector('#rangeGreen');
+    rangeBlue = document.querySelector('#rangeBlue');
+
+    inputRed = document.querySelector('#inputRed');
+    inputGreen = document.querySelector('#inputGreen');
+    inputBlue = document.querySelector('#inputBlue');
+    
+    rangeRed.addEventListener('input', setColor);
+    rangeGreen.addEventListener('input', setColor);
+    rangeBlue.addEventListener('input', setColor);
+
+    setColor();
+}
+
+function setColor(){
+    var red = +rangeRed.value;
+    var green = +rangeGreen.value;
+    var blue = +rangeBlue.value;
+
+    inputRed.value = red;
+    inputGreen.value = green;
+    inputBlue.value = blue;
+
+    var rgbCSS = 'rgb('+red+','+green+','+blue+')';
+
+    divSquare.style.backgroundColor = rgbCSS;
+}
+
